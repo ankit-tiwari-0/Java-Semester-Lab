53908a6aa4a1ecb4de2d67d46be56fab03038292
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-28 11:37:50 +0530

    basic arthmatic op

---
+++ 04-lecture/Javaoperators.java
@@ -0,0 +1,16 @@
+public class Javaoperators {
+      
+    public static void main(String[] args) {
+        //Arithmatic operators --> +, -, *, /, %, +=, -=, /=, %=, ++, --
+        int a = 5;
+        int b = 10;
+
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;
+        int g = a%b;
+
+        System.out.println(c + "," + d +"," + e +"," + f +"," + g  );
+    }
+}
