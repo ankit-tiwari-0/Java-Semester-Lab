9020e395cd400565fc8c42a0e59192184446d212
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-28 10:27:05 +0530

    Implicit conversion (byte to int)

---
+++ 03-lecture/Type_conversion.java
@@ -0,0 +1,10 @@
+public class Type_conversion {
+    public static void main(String[] args) {
+        // Implicit conversion (byte to int)
+        byte b = 24;
+        int i;
+
+        i = b;  // automatic type conversion
+        System.out.println(i);
+    }
+}
