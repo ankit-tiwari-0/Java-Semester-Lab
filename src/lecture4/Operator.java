e92c2e5fbe6ed911ad08dd5adaa11bc61fbf7ceb
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-01 21:32:48 +0530

    Arithmetic operator is completed

--- src/lecture4/Operator.java
+++ src/lecture4/Operator.java
@@ -20,6 +20,27 @@ public class Operator {
 
         h -= 2; // h = h - 2
         System.out.println(h);
+       
+        int i = 6;
+        i++; // here increase by 1, i = i + 1--> i +=1;
+        i--;
+
+        System.out.println(i);//7
+
+        //pre increment and post increament/decrement
+
+        int j = 7;
+        j++;//postfix increment
+        ++j;//prefix increment
+
+        // 9
+        int k = j++; //k = j; j = j +1;here first assiqn val to k then increase by 1
+        System.out.println(j+ "," + k);
+        int l = ++j; //l = j + 1;
+
+        System.out.println(j + "," +l);//11,11
+
+        
 
     }
 }
