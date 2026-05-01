0f06091d9626b18c11303ad9df7730c9aa7829ad
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-01 22:11:18 +0530

    relation opt is completed

--- src/lecture4/Relational.java
+++ src/lecture4/Relational.java
@@ -5,6 +5,22 @@ package lecture4;
 public class Relational {
     
     public static void main(String[] args){
+        
+      int a = 5;
+      int b = 10;
+      
+      boolean c = (a == b);//f
+      System.out.println(c);
+
+      boolean d = (a != b);//t
+      System.out.println(d);
+
+       boolean e = (a < b);//t
+       System.out.println(e);
+
+       boolean f = (a <= b);//t ek condition correct hona chahiye
+       System.out.println(f);
+
 
     }
 }
