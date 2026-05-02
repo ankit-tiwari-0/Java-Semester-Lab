bbb665f6ebb51ae7f661b4116e3c8149eb694e61
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-02 11:14:16 +0530

    Bitwise

--- src/lecture4/Bitwise.java
+++ src/lecture4/Bitwise.java
@@ -2,6 +2,16 @@ package lecture4;
 
 public class Bitwise {
    public static void main(String[] args) {
+    // Bitwise operations
+    int a = 2; // 10 ---> 00000000 00000000 00000000 00000010 
+    int b = 3; // 11 --> 00000000 00000000 00000000 00000011
+    int c = a & b; // 10 --> 2
+    int d = a | b; // 11 --> 3
+    int e = a^b; // 01 -- 1
+    int f = ~a; //
+
+    System.out.println(c + "," +d+","+e+","+f);
     
+
    } 
 }
