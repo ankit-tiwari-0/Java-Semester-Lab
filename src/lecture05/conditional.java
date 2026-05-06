251731d8873a15904b7f9d5a3fbfcb7f88741f20
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-06 22:08:02 +0530

    Conditional Statements if else

---
+++ src/lecture05/conditional.java
@@ -0,0 +1,24 @@
+package lecture05;
+
+public class conditional {
+     public static void main(String[] args) {
+
+        int i = 5;
+
+        if (i == 5) {
+            System.out.println("i is 5");
+        } else {
+            System.out.println("i is not");
+        }
+
+        int o = 6;
+         
+        if (o % 2 != 0) {
+            
+            System.out.println("o is even");
+        }
+        else{
+            System.out.println("o is odd");
+        }
+    } 
+}
