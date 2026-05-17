0756267386999c66b7e606adc5243d1cc2158590
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-17 22:26:20 +0530

    while loop

---
+++ src/06-lectuce/Loops.java
@@ -0,0 +1,22 @@
+public static void main(String[] args) {
+    int i = 10;
+
+    // Loops ---> while
+
+    while(i >= 1) {//doolean --> true / false
+
+        System.out.println(i);
+        i--;
+    }
+
+    int k = 0;
+    while (k-- > 10) {
+         System.out.println(k);
+    }
+   
+    int m = 0;
+    while (m++ < 10) {
+         System.out.println(m);
+    }
+
+}
